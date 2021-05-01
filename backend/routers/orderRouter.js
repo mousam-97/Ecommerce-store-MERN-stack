ec232fc59a465c0f81ffc62a3449c91642453b83
Mousam <mousam.kalita777@gmail.com>
2021-05-01 20:44:37 +0530

    Implement Place Order action

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -22,9 +22,11 @@ orderRouter.post(
         totalPrice: req.body.totalPrice,
         user: req.user._id,
       });
+      const createdOrder = await order.save();
+      res
+        .status(201)
+        .send({ message: "New Order Created", order: createdOrder });
     }
-    const createdOrder = await order.save();
-    res.status(201).send({ message: "New Order Created", order: createdOrder });
   })
 );
 
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,13 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { createOrder } from "../actions/orderActions";
 import CheckoutSteps from "../component/CheckoutSteps";
+import { ORDER_CREATE_RESET } from "../constants/orderConstants";
+import MessageBox from "../component/MessageBox";
+import LoadingBox from "../component/LoadingBox";
 
 function PlaceOrderScreen(props) {
   const cart = useSelector((state) => state.cart);
   if (!cart.paymentMethod) {
     props.history.push("/payment");
   }
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { loading, success, error, order } = orderCreate;
   const toPrice = (num) => Number(num.toFixed(2));
   cart.itemsPrice = toPrice(
     cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
@@ -15,9 +21,16 @@ function PlaceOrderScreen(props) {
   cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
   cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+  const dispatch = useDispatch();
   const placeOrderHandler = (e) => {
-    //dispatch placeorder Action
+    dispatch(createOrder({ ...cart, orderItems: cart.cartItems }));
   };
+  useEffect(() => {
+    if (success) {
+      props.history.push(`/order/${order._id}`);
+      dispatch({ type: ORDER_CREATE_RESET });
+    }
+  }, [dispatch, order, props.history, success]);
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -122,6 +135,8 @@ function PlaceOrderScreen(props) {
                   Place Order
                 </button>
               </li>
+              {loading && <LoadingBox></LoadingBox>}
+              {error && <MessageBox variant="danger">{error}</MessageBox>}
             </ul>
           </div>
         </div>
