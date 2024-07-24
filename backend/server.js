9449d618dbbd7242406261f87976911e1c87925e
MK <mousamdreamer@gmail.com>
2024-07-25 00:11:16 +0530

    make code ready to deploy to vercel

--- backend/server.js
+++ backend/server.js
@@ -26,19 +26,23 @@ app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is ready");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Server is ready");
+//   });
+// }
+
+app.get("/", (req, res) => {
+	res.send("Server is ready");
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
