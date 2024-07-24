edd34b058b8aa22e89d74f9fdae4467a2845b600
MK <mousamdreamer@gmail.com>
2024-07-25 02:00:10 +0530

    fix env

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,45 +8,45 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import CarouselScreen from "../component/Carousel";
 
 function HomeScreen() {
-  const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 
-  useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(listProducts());
+	}, [dispatch]);
 
-  console.log(products);
+	// console.log(products);
 
-  return (
-    <div className="home">
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-        <div className="row center">
-          <div className="carousal__banner">
-            <CarouselScreen />
-          </div>
+	return (
+		<div className="home">
+			{loading ? (
+				<LoadingBox></LoadingBox>
+			) : error ? (
+				<MessageBox variant="danger">{error}</MessageBox>
+			) : (
+				<div className="row center">
+					<div className="carousal__banner">
+						<CarouselScreen />
+					</div>
 
-          {products.map((product) => {
-            return (
-              <Product
-                _id={product._id}
-                key={product._id}
-                image={product.image}
-                name={product.name}
-                price={product.price}
-                rating={product.rating}
-                numReviews={product.numReviews}
-              />
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+					{products.map((product) => {
+						return (
+							<Product
+								_id={product._id}
+								key={product._id}
+								image={product.image}
+								name={product.name}
+								price={product.price}
+								rating={product.rating}
+								numReviews={product.numReviews}
+							/>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default HomeScreen;
--- frontend/src/utils/apiUrlUtils.js
+++ frontend/src/utils/apiUrlUtils.js
@@ -1,7 +1,7 @@
-const API_URL = process.env.ROOT_API;
+const API_URL = process.env.REACT_APP_ROOT_API;
 
 export function getApiRoot() {
-	if (process.env.PRODUCTION === true) {
+	if (process.env.REACT_APP_PRODUCTION === "true") {
 		return API_URL;
 	}
 
